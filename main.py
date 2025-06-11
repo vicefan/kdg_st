b09fb13fa393237e827fe6a44fff9fdd4557d1f4
vicefan <dddooong2000@naver.com>
2025-06-11 14:26:36 +0900

    v2

--- main.py
+++ main.py
@@ -14,5 +14,5 @@ else:
 
 edges = cv2.Canny(np.array(image), 100, 200)
 tab1, tab2 = st.tabs(["Detected edges", "Original"])
-tab1.image(edges, use_column_width=True)
-tab2.image(image, use_column_width=True)
+tab1.image(edges, use_container_width=True)
+tab2.image(image, use_container_width=True)
